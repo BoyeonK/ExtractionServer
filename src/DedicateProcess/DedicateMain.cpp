457ee350d6f1aba0702d7e1dbbfd58ec80f88ff9
kby <tetepiti149@gmail.com>
2026-05-16 14:14:04 +0900

    DedicateProcess에서 Redis핸들 사용 안함.

--- src/DedicateProcess/DedicateMain.cpp
+++ src/DedicateProcess/DedicateMain.cpp
@@ -14,19 +14,14 @@
 int DedicateMain(int argc, char* argv[]) {
     std::cout << "D1 - OK : 인게임 프로세스 부팅 완료" << std::endl;
 
-    const char* env_redis_host  = std::getenv("REDIS_HOST");
-    const char* env_redis_port  = std::getenv("REDIS_PORT");
-    std::string redis_url = "tcp://" + std::string(env_redis_host) + ":" + std::string(env_redis_port);
-
     try {
         IORing = new IoUringWrapper();
-        pRedis = new sw::redis::Redis(redis_url);
     } catch (const std::exception& e) {
-        std::cerr << "D2 - X : 환경변수, IoUring객체, Redis핸들 중에서 최소 하나 실패" << std::endl;
+        std::cerr << "D2 - X : IoUring객체 생성 실패" << std::endl;
         return 1;
     }
 
-    std::cout << "D2 - OK : 인게임 프로세스에서 환경변수 로드, IoUring객체 및 Redis핸들 생성" << std::endl;
+    std::cout << "D2 - OK : 인게임 프로세스에서 IoUring객체 생성" << std::endl;
     
     PacketHandler::Init();
 
