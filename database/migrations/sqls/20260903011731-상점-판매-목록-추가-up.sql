4c68b45dd4218e0fe5237eaea9de46f3cd05dc19
kbyGram <tetepiti149@gmail.com>
2026-09-03 10:21:32 +0900

    상점 판매 목록 추가

---
+++ database/migrations/sqls/20260903011731-상점-판매-목록-추가-up.sql
@@ -0,0 +1,11 @@
+--  Auto-generated SQL script #202609031015
+INSERT INTO shop_items (item_id)
+	VALUES (2);
+INSERT INTO shop_items (item_id)
+	VALUES (3);
+INSERT INTO shop_items (item_id)
+	VALUES (4);
+INSERT INTO shop_items (item_id)
+	VALUES (5);
+INSERT INTO shop_items (item_id)
+	VALUES (6);
